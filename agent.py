a3b7d96df952f9be86e466aab41796af55be1e97
bipin9235 <87981816+bipin9235@users.noreply.github.com>
2025-07-10 12:57:41 +0000

    updated agent file

--- agent.py
+++ agent.py
@@ -1,11 +1,12 @@
 from autogen import ConversableAgent
-import streamlit as st
 
+import streamlit as st
 model = "openai/gpt-4.1-Nano"
 llm_config = {
     "model": model,
     'base_url':'https://openrouter.ai/api/v1',
     'api_key':st.secrets["OPENAI_API_KEY"],
+    #'api_key':OPENAI_API_KEY,
     
 }
 def ask_agent(msg):
@@ -17,4 +18,4 @@ def ask_agent(msg):
     response=agent.generate_reply(
             messages=[{'role':'user','content':{msg}},{'role':'system','content':'You are a Software Engineer, reply each ask in engineering context.'}]
             )
-    return response['content']
+    return response
